f89ad0a4e2babdd0425f56c6be547c6552968a38
Nata Vacheishvili <natashkela@gmail.com>
2023-08-23 18:22:50 -0400

    header done

--- App.js
+++ App.js
@@ -1,11 +1,21 @@
 import React from 'react';
-import {SafeAreaView} from 'react-native';
+import {SafeAreaView, TouchableOpacity, View, Text} from 'react-native';
 import Title from './components/Title/Title';
-
+import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
+import {faEnvelope} from '@fortawesome/free-solid-svg-icons';
+import globalStyle from './assets/styles/globalStyle';
 const App = () => {
   return (
     <SafeAreaView>
-      <Title title={'Let’s Explore'} />
+      <View style={globalStyle.header}>
+        <Title title={'Let’s Explore'} />
+        <TouchableOpacity style={globalStyle.messageIcon}>
+          <FontAwesomeIcon icon={faEnvelope} size={20} color={'#898DAE'} />
+          <View style={globalStyle.messageNumberContainer}>
+            <Text style={globalStyle.messageNumber}>2</Text>
+          </View>
+        </TouchableOpacity>
+      </View>
     </SafeAreaView>
   );
 };
---
+++ assets/styles/globalStyle.js
@@ -0,0 +1,37 @@
+import {StyleSheet} from 'react-native';
+import {getFontFamily} from '../fonts/helper';
+
+const globalStyle = StyleSheet.create({
+  header: {
+    marginLeft: 27,
+    marginRight: 17,
+    marginTop: 30,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  messageIcon: {
+    padding: 14,
+    borderRadius: 100,
+    backgroundColor: '#F9FAFB',
+  },
+  messageNumberContainer: {
+    backgroundColor: '#F35BAC',
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'row',
+    width: 10,
+    height: 10,
+    borderRadius: 10,
+    position: 'absolute',
+    right: 10,
+    top: 12,
+  },
+  messageNumber: {
+    color: '#FFFFFF',
+    fontSize: 6,
+    fontFamily: getFontFamily('Inter', '600'),
+  },
+});
+
+export default globalStyle;
